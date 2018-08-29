eecd562ff0f05ac519fa57d2ed0d490bb5952db9
Michal Gallovič <gallovicm@gmail.com>
2018-08-29 16:48:17 +0200

    Add CustomControlledComponents App

---
+++ src/CustomControlledComponentsApp.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app" class="container bg-presentation">
+		
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			
+		}
+	}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import App from './NativeControlledComponentsApp.vue'
+// import App from './NativeControlledComponentsApp.vue'
+import App from './CustomControlledComponentsApp.vue'
 
 Vue.config.productionTip = false
 
